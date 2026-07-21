74c25453916c259aadb31503dff094b4d178b2f8
JOSH-ITHA <upatijoshitha@gmail.com>
2026-07-21 20:07:26 +0530

    modules,routes,nodemon

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-const express=require('express');
+import express from 'express';
+import add from './math.js';
 const app=express();
-const PORT=3000;
-
-
-//Home Route
+const port=3000;
 app.get('/',(req,res)=>{
-    res.send("Hello world!Express Tutorial");
+    res.send('Hello ExpressJS');
+    console.log(add(2,3));
 })
-app.listen(PORT,()=>{
-    console.log('Server is running on http://localhost:3000');
-});
+app.get('/about',(req,res)=>{
+    res.end('Hello About ExpressJS');
+})
+app.get('/profile',(req,res)=>{
+    res.send('<h1>Hello Profile ExpressJS<h1>');
+})
+
+app.listen(port,()=>{
+    console.log('App listening on port 3000');
+})
