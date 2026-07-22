75d8a78fa628ae8e8f180aac79452957db128da3
JOSH-ITHA <upatijoshitha@gmail.com>
2026-07-22 19:48:26 +0530

    render HTML File in express.js

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
+// 
 import express from 'express';
-import add from './math.js';
-const app=express();
-const port=3000;
-app.get('/',(req,res)=>{
-    res.send('Hello ExpressJS');
-    console.log(add(2,3));
-})
-app.get('/about',(req,res)=>{
-    res.end('Hello About ExpressJS');
-})
-app.get('/profile',(req,res)=>{
-    res.send('<h1>Hello Profile ExpressJS<h1>');
-})
+import path from 'path';
 
-app.listen(port,()=>{
-    console.log('App listening on port 3000');
+
+const app=express();
+const PORT=3000;
+app.get('/',(req,res)=>{
+    console.log(path.resolve('index.html'));
+    const filepath=path.resolve('index.html');
+    res.sendFile(filepath);
+})
+app.listen(PORT,()=>{
+    console.log('Server is running on http://localhost:3000');
+
 })
